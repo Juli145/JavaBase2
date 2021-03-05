beecc1fae77f353584645e4f9160b297446b8804
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-03-05 21:42:45 +0200

    Updated

--- src/com/company/CalcAPI.java
+++ src/com/company/CalcAPI.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class CalcAPI {
+    public static double calc(double num1, double num2, char operation) {
+        double result = 0;
+        switch (operation) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num1 / num2; break;
+        }
+        return result;
+    }
 }
--- src/com/company/SimpleCalc.java
+++ src/com/company/SimpleCalc.java
@@ -8,23 +8,47 @@ import java.awt.event.ActionListener;
 public class SimpleCalc {
     private JFrame window;
     private JPanel panel;
-    private JTextField textAreaNum1, textAreaNum2, textAreaAction, textAreaResult;
+    private JLabel labelNumber1, labelNumber2, labelMathOperation, labelResult;
+    private JTextField textFieldNum1, textFieldNum2, textFieldMathOperation, textAreaResult;
     private JButton button;
 
     public SimpleCalc(){ constructAll();}
 
     public void constructAll(){
         constructWindow();
+        constructLabels();
         constructTextFields();
         constructButton();
         constructFieldResult();
         window.setVisible(true);
     }
+    private void getResult() {
+        double num1 = 0, num2 = 0;
+        char mathOperation = ' ';
+        try {
+            num1 = Double.parseDouble(textFieldNum1.getText());
+            num2 = Double.parseDouble(textFieldNum2.getText());
+            mathOperation = textFieldMathOperation.getText().charAt(0);
+        } catch (NullPointerException e) {
+            textAreaResult.setText("Enter number 1 and number 2");
+            throw new NullPointerException();
+        } catch (NumberFormatException e) {
+            textAreaResult.setText("Incorrect data");
+            throw new NumberFormatException();
+        }
+
+        double result;
+            result = CalcAPI.calc(num1, num2, mathOperation);
+            String answer = String.valueOf(result);
+            textAreaResult.setText(answer);
+    }
+
 
     public void constructWindow(){
         JFrame.setDefaultLookAndFeelDecorated(true);
         window = new JFrame("Calculator");
         window.setSize(500, 700);
+        window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new JPanel();
         panel.setLayout(new BorderLayout());
@@ -34,16 +58,31 @@ public class SimpleCalc {
         window.setLocationRelativeTo(null);
     }
 
+    private void constructLabels() {
+        labelNumber1 = new JLabel("Number 1");
+        labelNumber1.setBounds(45, 50, 100, 40);
+        panel.add(labelNumber1);
+        labelNumber2 = new JLabel("Number 2");
+        labelNumber2.setBounds(45, 110, 100, 40);
+        panel.add(labelNumber2);
+        labelMathOperation = new JLabel("Math operation");
+        labelMathOperation.setBounds(45, 170, 100, 40);
+        panel.add(labelMathOperation);
+        labelResult = new JLabel("Result");
+        labelResult.setBounds(45, 290, 100, 40);
+        panel.add(labelResult);
+    }
+
     private void constructTextFields() {
-        textAreaNum1 = new JTextField();
-        textAreaNum1.setBounds(150, 50, 190, 40);
-        panel.add(textAreaNum1);
-        textAreaNum2 = new JTextField();
-        textAreaNum2.setBounds(150, 110, 190, 40);
-        panel.add(textAreaNum2);
-        textAreaAction = new JTextField();
-        textAreaAction.setBounds(150, 170, 190, 40);
-        panel.add(textAreaAction);
+        textFieldNum1 = new JTextField();
+        textFieldNum1.setBounds(150, 50, 190, 40);
+        panel.add(textFieldNum1);
+        textFieldNum2 = new JTextField();
+        textFieldNum2.setBounds(150, 110, 190, 40);
+        panel.add(textFieldNum2);
+        textFieldMathOperation = new JTextField();
+        textFieldMathOperation.setBounds(150, 170, 190, 40);
+        panel.add(textFieldMathOperation);
     }
 
     public void constructButton(){
@@ -62,6 +101,7 @@ public class SimpleCalc {
     class MyActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+            getResult();
         }
     }
 }
